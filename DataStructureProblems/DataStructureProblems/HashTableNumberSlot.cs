6fa69713aa25236dd31f589358a84e32cb23b21a
Piyushnmj <piyushnmj@yahoo.com>
2022-11-30 19:16:14 +0530

    Hashing function to search a number in a slot

---
+++ DataStructureProblems/DataStructureProblems/HashTableNumberSlot.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+
+namespace DataStructureProblems
+{
+    public class HashTableNumberSlot
+    {
+        public string FilePath = @"F:\Bridgelabz Codin\Data-Structure-Problems\DataStructureProblems\DataStructureProblems\NumberFile.json";
+
+        Hashtable objHashtable = new Hashtable();
+
+        public void SearchNumbersInHashTable(string value)
+        {
+            bool dataExists = false;
+            string numToRead = File.ReadAllText(FilePath);
+            string numData = JsonConvert.DeserializeObject<string>(numToRead);
+
+            string[] numList = numData.Split(' ');
+
+            for (int i = 0; i< numList.Length; i++)
+            {
+                objHashtable.Add(i, numList[i]);
+            }
+
+            Display();
+
+            foreach (var key in objHashtable.Keys)
+            {
+                if (objHashtable[key].Equals(value))
+                {
+                    dataExists = true;
+                    Console.WriteLine($"{objHashtable[key]} exists");
+                    Console.WriteLine($"{objHashtable[key]} removed");
+                    objHashtable.Remove(key);
+                    Console.WriteLine();
+                    break;
+                }
+            }
+            if (!dataExists)
+            {
+                Console.WriteLine($"{value} does not exist");
+                int addKey = objHashtable.Count + 1;
+                objHashtable.Add(addKey, value);
+                Console.WriteLine($"{value} added");
+            }
+
+            string[] joinString= new string[objHashtable.Count];
+            int j = 0;
+            foreach (var key in objHashtable.Keys)
+            {
+                string result = Convert.ToString(objHashtable[key]);
+                joinString[j] = result;
+                j++;
+            }
+
+            string combinedString = string.Join(" ", joinString);
+            string numToAdd = JsonConvert.SerializeObject(combinedString);
+            File.WriteAllText(FilePath, numToAdd);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\nElements in Hashtable are: ");
+            foreach (var key in objHashtable.Keys)
+            {
+                Console.WriteLine($"Slot-{key} contains number {objHashtable[key]}");
+            }
+        }
+    }
+}
--- DataStructureProblems/DataStructureProblems/Program.cs
+++ DataStructureProblems/DataStructureProblems/Program.cs
@@ -6,10 +6,13 @@
         {
             Console.WriteLine("Welcome to Data Structure Program");
 
-            Console.WriteLine("Enter a number to search: ");
-            int numSearch = Convert.ToInt32(Console.ReadLine());
-            OrderedList objList = new OrderedList();
-            objList.SearchNumbersInOrderedList(numSearch);
+            Console.WriteLine("\nEnter a number to search: ");
+            string numSearch = Console.ReadLine();
+            HashTableNumberSlot objTable = new HashTableNumberSlot();
+            Console.WriteLine("\n==========Before Operation==========");
+            objTable.SearchNumbersInHashTable(numSearch);
+            Console.WriteLine("\n==========After Operation==========");
+            objTable.Display();
             Console.ReadLine();
         }
     }
